a4c9e1c2875cc04e6e6711f9a2edf8dc75baffc4
Vlad Zhelevsky <124770936+vladjelevsky@users.noreply.github.com>
2024-10-21 01:20:45 +0300

    update js files

---
+++ js/task-2.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function formatMessage(message, maxLength) {
+  const messageLength = message.length;
+  const cutMessage = message.slice(0, maxLength);
+
+  return messageLength <= maxLength ? `"${message}"` : `"${cutMessage}..."`;
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
